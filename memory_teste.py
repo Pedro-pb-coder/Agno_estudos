69799fdeccb818a69d210271629b4321da20d66e
Pedro-pb-coder <pedro.pe.madureira@gmail.com>
2026-01-16 15:30:55 -0300

    algumas melhorias nos agentes

---
+++ memory_teste.py
@@ -0,0 +1,24 @@
+from agno.agent import Agent
+from agno.db.sqlite import SqliteDb
+
+# Setup the SQLite database
+db = SqliteDb(db_file="tmp/data.db")
+
+# Setup a basic agent with the SQLite database
+agent = Agent(
+    db=db,
+    enable_user_memories=True,
+    markdown=True,
+    debug_mode=True,
+)
+
+while True:
+    pergunta = input("\nVocê: ")
+    if pergunta.lower() in ["sair", "exit", "quit"]:
+        break
+    
+    # O agente processa a pergunta e busca no DuckDB
+    agent.print_response(pergunta, stream=True)
+
+#agent.print_response("My name is John Doe and I like to play basketball on the weekends.")
+#agent.print_response("What's do I do in weekends?")
--- teste_agente.py
+++ teste_agente.py
@@ -10,16 +10,21 @@ from dotenv import load_dotenv
 import os
 # Verificação de segurança da API Key
 load_dotenv()
+
 # Verificação de segurança da API Key
 if not os.getenv("OPENAI_API_KEY"):
     print("❌ Erro: OPENAI_API_KEY não encontrada no arquivo .env")
     exit()
 
+if not os.getenv("db_url"):
+    print("❌ Erro: db_url não encontrada no arquivo .env")
+    exit()
 
-db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+db_url = os.getenv("db_url")
+
 
 # Create Knowledge Instance
-knowledge = Knowledge(
+knowledge_base = Knowledge(
     name="YouTube Knowledge Base",
     description="Knowledge base from YouTube video transcripts",
     vector_db=PgVector(
@@ -30,20 +35,45 @@ knowledge = Knowledge(
 
 
 # Add YouTube video content synchronously
-knowledge.add_content(
+knowledge_base.add_content(
     metadata={"source": "youtube", "type": "educational"},
-    url="https://www.youtube.com/watch?v=6pJ4o3jJ2cU&t=4s",
+    #url="https://www.youtube.com/watch?v=6pJ4o3jJ2cU", # efap 368
+    #url="https://www.youtube.com/watch?v=9H8EJLN9qXU", # efap 369
+    url="https://www.youtube.com/watch?v=I8U6kjqLkJQ", # Randon Game of thrones
+    skip_if_exists= True,
+    
+    reader=YouTubeReader(),
+)
+knowledge_base.add_content(
+    metadata={"source": "youtube", "type": "educational"},
+    url="https://www.youtube.com/watch?v=6pJ4o3jJ2cU",
+    skip_if_exists= True,
 
     reader=YouTubeReader(),
 )
 
+knowledge_base.add_content(
+    metadata={"source": "youtube", "type": "educational"},
+    url="https://www.youtube.com/watch?v=9H8EJLN9qXU",
+    skip_if_exists= True,
+
+    reader=YouTubeReader(),
+)
+#knowledge_base.load(recreate=False) # não funciona na versão atual 
+
 # Create an agent with the knowledge
 agent = Agent(
     model= OpenAIChat(id="gpt-5-nano",
-                      instructions=["fale primeiro quais são os dados que existem em knowkedge"]
+                      instructions=[
+                          "fale primeiro quais são os dados que existem em knowkedge",
+                          "reconheça a diferença entre cada um dos locutores do podcast",
+                          "Sempre diga se a informação veio do Knowledge Base ou da sua base de treinamento.",
+                          "Se encontrar a resposta no Knowledge Base, cite trechos específicos.",
+
+                          ]
                       ),
     tools=[OpenAITools(all),DuckDuckGoTools()],
-    knowledge=knowledge,
+    knowledge=knowledge_base,
     search_knowledge=True,
     markdown=True,
     debug_mode=True,
